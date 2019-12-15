8bd62a9c5b778a1ef7b9acb18a7c9b168de4f398
六翁 <lu.hl@alibaba-inc.com>
2019-12-15 16:38:56 +0800

    init

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"time"
+
+	"github.com/feuyeux/hello-rsocket/requester"
+	"github.com/feuyeux/hello-rsocket/responder"
+)
+
+func main() {
+	go responder.Start()
+
+	requester.ExecRequestResponse()
+	time.Sleep(100 * time.Millisecond)
+
+	requester.ExecFireAndForget()
+	time.Sleep(200 * time.Millisecond)
+
+	requester.ExecRequestStream()
+	time.Sleep(200 * time.Millisecond)
+
+	go requester.ExecRequestChannel()
+	time.Sleep(2 * time.Second)
+}
